a3faf720a63909609e7344017928147854d60e1e
Nicolás Agudelo <nicolasagudelolopez@gmail.com>
2026-01-20 12:52:14 -0500

    dejé lista la pantalla de animated_screen y configuré las rutas para esta

--- Proyectos/Flutter/widgets_app/lib/config/menu/menu_items.dart
+++ Proyectos/Flutter/widgets_app/lib/config/menu/menu_items.dart
@@ -39,4 +39,10 @@ MenuItem(
     link: '/snackbars',
     icon:Icons.info_outline
   ),
+MenuItem(
+    title: 'Animated container',
+    subTitle: 'Statefull widwet animado',
+    link: '/animated',
+    icon:Icons.check_box_outline_blank_rounded
+  ),
 ];
