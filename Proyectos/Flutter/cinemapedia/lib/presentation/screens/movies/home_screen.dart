cce49e1de6f6b8236ea36a8aa16ad9ad16538c18
Nicolás Agudelo <nicolasagudelolopez@gmail.com>
2026-01-26 13:41:29 -0500

    config(api): implement dio paths

--- Proyectos/Flutter/cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ Proyectos/Flutter/cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -11,9 +11,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(Enviroment.themovieDbKey)
-        ),
+      body: Placeholder()
     );
   }
 }
