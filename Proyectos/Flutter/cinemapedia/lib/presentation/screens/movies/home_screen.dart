686c2112b22cc6fea24a50c99198e81e0fd3d1ce
Nicolás Agudelo <nicolasagudelolopez@gmail.com>
2026-02-06 19:33:46 -0500

    feat(UI): implement bottom navigation bar

--- Proyectos/Flutter/cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ Proyectos/Flutter/cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -13,6 +13,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: CustomBottomNavigationBar(),
       body: _HomeView()
     );
   }
---
+++ Proyectos/Flutter/cinemapedia/lib/presentation/widgets/shared/custom_bottom_navigation_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      elevation: 0,
+      items: const[
+        BottomNavigationBarItem(icon:Icon(Icons.home),
+        label: 'Inicio',
+        ),
+
+        BottomNavigationBarItem(icon:Icon(Icons.label_outline),
+        label: 'Categorías',
+        ),
+
+        BottomNavigationBarItem(icon:Icon(Icons.favorite_outline),
+        label: 'Favoritos',
+        ),
+      ]);
+  }
+}
