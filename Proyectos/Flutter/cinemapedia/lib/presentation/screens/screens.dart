c66915ec6da249e3519ab45512250c07bcbc6654
Nicolás Agudelo <nicolasagudelolopez@gmail.com>
2026-02-19 18:27:49 -0500

    config(routes): config movie screen route

--- Proyectos/Flutter/cinemapedia/lib/presentation/screens/screens.dart
+++ Proyectos/Flutter/cinemapedia/lib/presentation/screens/screens.dart
@@ -1,2 +1,2 @@
+export 'movies/movie_screen.dart';
 export 'package:cinemapedia/presentation/screens/movies/home_screen.dart';
-
--- Proyectos/Flutter/cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ Proyectos/Flutter/cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieHorizontalListview extends StatefulWidget {
 
@@ -107,7 +108,11 @@ class _Slide extends StatelessWidget {
                       child: Center(child: const CircularProgressIndicator(strokeWidth:2 ,)),
                     );
                   }
-                  return FadeIn(child: child);
+                  return 
+                  GestureDetector(
+                    child: FadeIn(child: child),
+                    onTap: () => context.push('/movie/${movie.id}'),
+                    );
                 },
               ),
             ),
